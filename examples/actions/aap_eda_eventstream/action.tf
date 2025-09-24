1d6c452941ade46d95b01689caca83608a0814fe
Dan Leehr <dleehr@redhat.com>
2025-09-23 20:19:01 -0400

    Trying actions docs

---
+++ examples/actions/aap_eda_eventstream/action.tf
@@ -0,0 +1,23 @@
+resource "terraform_data" "trigger" {
+  input = "%s"
+  lifecycle {
+    action_trigger {
+      events  = [after_create]
+      actions = [action.aap_eda_eventstream.create]
+    }
+  }
+}
+
+action "aap_eda_eventstream" "create" {
+  config {
+    limit             = "infra"
+    template_type     = "job"
+    job_template_name = "After Create Job Template"
+    organization_name = "Default"
+    event_stream_config = {
+      username = "event-stream-username"
+      password = "event-stream-password"
+      url      = "http://aap-event-stream-url.example.com/"
+    }
+  }
+}
